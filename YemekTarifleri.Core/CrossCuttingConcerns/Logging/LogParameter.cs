5ad629b5473c0317ed8cc8fe37644f85bb99d575
yunver <yunus.unver1995@gmail.com>
2019-06-07 00:44:01 +0300

    Add LogParameter and LogDetail class.

---
+++ YemekTarifleri.Core/CrossCuttingConcerns/Logging/LogParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekTarifleri.Core.CrossCuttingConcerns.Logging
+{
+    public class LogParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Object Value { get; set; }
+    }
+}
