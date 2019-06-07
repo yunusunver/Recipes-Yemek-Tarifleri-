7e543602dd51b07ca034e1a4ebfc1ff4a7ba147d
yunver <yunus.unver1995@gmail.com>
2019-06-07 09:50:09 +0300

    Add Log4Net Implementation

---
+++ YemekTarifleri.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace YemekTarifleri.Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerService
+    {
+        private ILog _log;
+
+        public LoggerService(ILog log)
+        {
+            _log = log;
+        }
+
+        public bool isInfoEnabled => _log.IsInfoEnabled;
+        public bool isDebugEnabled => _log.IsDebugEnabled;
+        public bool isWarnEnabled => _log.IsWarnEnabled;
+        public bool isFatalEnabled => _log.IsFatalEnabled;
+        public bool isErrorEnabled => _log.IsErrorEnabled;
+
+        public void Info(object logMessage)
+        {
+            if (isInfoEnabled)
+            {
+                _log.Info(logMessage);
+            }
+        }
+
+        public void Debug(object logMessage)
+        {
+            if (isDebugEnabled)
+            {
+                _log.Debug(logMessage);
+            }
+        }
+
+        public void Warn(object logMessage)
+        {
+            if (isWarnEnabled)
+            {
+                _log.Warn(logMessage);
+            }
+        }
+
+        public void Fatal(object logMessage)
+        {
+            if (isFatalEnabled)
+            {
+                _log.Fatal(logMessage);
+            }
+        }
+
+        public void Error(object logMessage)
+        {
+            if (isErrorEnabled)
+            {
+                _log.Error(logMessage);
+            }
+        }
+
+
+    }
+}
---
+++ YemekTarifleri.Core/CrossCuttingConcerns/Logging/Log4Net/SerializableLogEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+
+namespace YemekTarifleri.Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    [Serializable]
+    public class SerializableLogEvent
+    {
+        private LoggingEvent _loggingEvent;
+
+        public SerializableLogEvent(LoggingEvent loggingEvent)
+        {
+            _loggingEvent = loggingEvent;
+        }
+
+        public string Username => _loggingEvent.UserName;
+        public object Messageobjects => _loggingEvent.MessageObject;
+    }
+}
