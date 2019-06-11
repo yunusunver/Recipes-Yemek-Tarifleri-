8d43c2ad7aa4c9942e95046b00235665f6efdd5a
yunver <yunus.unver1995@gmail.com>
2019-06-11 23:49:07 +0300

    Web API Implementation

---
+++ YemekTarifleri.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YemekTarifleri.Business.Abstract;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.WebApi.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public List<Category> Get()
+        {
+            return _categoryService.GetAll();
+        }
+    }
+}
