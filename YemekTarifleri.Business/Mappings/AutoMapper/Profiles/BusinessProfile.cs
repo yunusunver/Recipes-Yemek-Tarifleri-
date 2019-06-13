529a37c3c034d4c4b7315e225d6378ccb16eb4ee
yunver <yunus.unver1995@gmail.com>
2019-06-13 15:44:42 +0300

    AutoMapper Profile Implementation

---
+++ YemekTarifleri.Business/Mappings/AutoMapper/Profiles/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.Business.Mappings.AutoMapper.Profiles
+{
+    public class BusinessProfile:Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<Category, Category>();
+        }
+    }
+}
