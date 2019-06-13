b5c52b1810e040c477ef325d2643caaa3a1f59f7
yunver <yunus.unver1995@gmail.com>
2019-06-13 15:34:39 +0300

    AutoMapper IoC Implementation

---
+++ YemekTarifleri.Business/DependencyResolvers/Ninject/AutoMapperModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Ninject.Modules;
+
+namespace YemekTarifleri.Business.DependencyResolvers.Ninject
+{
+    public class AutoMapperModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMapper>().ToConstant(CreateConfiguration().CreateMapper());
+        }
+
+        private MapperConfiguration CreateConfiguration()
+        {
+            var config=new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfiles(GetType().Assembly);
+            });
+
+            return config;
+        }
+    }
+}
