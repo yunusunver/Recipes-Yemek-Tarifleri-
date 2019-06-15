e9adf93aea69cdf9949b692bcb6712588a1e56ff
yunver <yunus.unver1995@gmail.com>
2019-06-16 00:01:38 +0300

    Added Meal View Model Class

---
+++ YemekTarifleri.MvcWebUI/Models/MealViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.MvcWebUI.Models
+{
+    public class MealViewModel
+    {
+        public List<Meal> Meals { get; set; }
+    }
+}
