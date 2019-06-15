c26a3973907d471e824757615d53396cf7bf878b
yunver <yunus.unver1995@gmail.com>
2019-06-15 23:58:16 +0300

    Added Meal Controller in Web UI Layer

---
+++ YemekTarifleri.MvcWebUI/Controllers/MealController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YemekTarifleri.Business.Abstract;
+
+namespace YemekTarifleri.MvcWebUI.Controllers
+{
+    public class MealController : Controller
+    {
+        private IMealService _mealService;
+
+        public MealController(IMealService mealService)
+        {
+            _mealService = mealService;
+        }
+
+        // GET: Meal
+        public ActionResult Index()
+        {
+            var model = new MealViewModel
+            {
+                Meals=_mealService.GetAll()
+            };
+            return View();
+        }
+    }
+}
