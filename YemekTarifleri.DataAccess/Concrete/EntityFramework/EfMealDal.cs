e472456a65c515034eed140fa521f7b5a377bede
yunver <yunus.unver1995@gmail.com>
2019-06-15 23:03:14 +0300

    Meal Interface and Class added Entity Framework and NHibernate in Data Access Layer

---
+++ YemekTarifleri.DataAccess/Concrete/EntityFramework/EfMealDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YemekTarifleri.Core.DataAccess.EntityFramework;
+using YemekTarifleri.DataAccess.Abstract;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.DataAccess.Concrete.EntityFramework
+{
+    public class EfMealDal:EfEntityRepositoryBase<Meal,FoodContext>,IMealDal
+    {
+    }
+}
---
+++ YemekTarifleri.DataAccess/Concrete/NHibernate/NhMealDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YemekTarifleri.Core.DataAccess.NHibernate;
+using YemekTarifleri.DataAccess.Abstract;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.DataAccess.Concrete.NHibernate
+{
+    public class NhMealDal:NhEntityRepositoryBase<Meal>,IMealDal
+    {
+        public NhMealDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
