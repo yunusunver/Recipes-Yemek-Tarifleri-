f9f12668a5983e6ca309de150f464991d5d4262e
yunver <yunus.unver1995@gmail.com>
2019-06-15 23:55:11 +0300

    Added Meals Controller in Web API Layer

---
+++ YemekTarifleri.WebApi/Controllers/MealsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YemekTarifleri.Business.Abstract;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.WebApi.Controllers
+{
+    public class MealsController : ApiController
+    {
+        private IMealService _mealService;
+
+        public MealsController(IMealService mealService)
+        {
+            _mealService = mealService;
+        }
+
+        public List<Meal> Get()
+        {
+            return _mealService.GetAll();
+        }
+    }
+}
