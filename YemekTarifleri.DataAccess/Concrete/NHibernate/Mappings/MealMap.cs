f29304c2a83e0a7c5f5f5836d78743b2f04be8a2
yunver <yunus.unver1995@gmail.com>
2019-06-17 00:27:45 +0300

    Added Meal Properties Mapping NHibernate

--- YemekTarifleri.DataAccess/Concrete/NHibernate/Mappings/MealMap.cs
+++ YemekTarifleri.DataAccess/Concrete/NHibernate/Mappings/MealMap.cs
@@ -15,6 +15,16 @@ namespace YemekTarifleri.DataAccess.Concrete.NHibernate.Mappings
             Table(@"Meal");
             LazyLoad();
             Id(x => x.Id).Column("Id");
+            Map(x => x.CategoryId).Column("CategoryId");
+            Map(x => x.UserId).Column("UserId");
+            Map(x => x.CookingTime).Column("CookingTime");
+            Map(x => x.Description).Column("Description");
+            Map(x => x.CreatedDate).Column("CreatedDate");
+            Map(x => x.Image).Column("Image");
+            Map(x => x.PersonQuantity).Column("PersonQuantity");
+            Map(x => x.Name).Column("Name");
+            Map(x => x.Point).Column("Point");
+            Map(x => x.PreparationTime).Column("PreparationTime");
         }
     }
 }
