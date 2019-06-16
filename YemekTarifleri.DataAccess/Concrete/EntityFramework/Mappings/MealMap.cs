ac4c51e82b3ab1ffac2d6faff9ed695666d5da56
yunver <yunus.unver1995@gmail.com>
2019-06-17 00:23:32 +0300

    Added Meal Properties Mapping

--- YemekTarifleri.DataAccess/Concrete/EntityFramework/Mappings/MealMap.cs
+++ YemekTarifleri.DataAccess/Concrete/EntityFramework/Mappings/MealMap.cs
@@ -15,6 +15,17 @@ namespace YemekTarifleri.DataAccess.Concrete.EntityFramework.Mappings
             ToTable(@"Meal", @"dbo");
             HasKey(x=>x.Id);
 
+            Property(x => x.Id).HasColumnName("Id");
+            Property(x => x.CategoryId).HasColumnName("CategoryId");
+            Property(x => x.UserId).HasColumnName("UserId");
+            Property(x => x.CookingTime).HasColumnName("CookingTime");
+            Property(x => x.Description).HasColumnName("Description");
+            Property(x => x.CreatedDate).HasColumnName("CreatedDate");
+            Property(x => x.Image).HasColumnName("Image");
+            Property(x => x.PersonQuantity).HasColumnName("PersonQuantity");
+            Property(x => x.Name).HasColumnName("Name");
+            Property(x => x.Point).HasColumnName("Point");
+            Property(x => x.PreparationTime).HasColumnName("PreparationTime");
         }
     }
 }
