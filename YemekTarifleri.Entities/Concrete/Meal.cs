a9acecb7e11f1d3c2aca56cd24c9f2994af6eb0f
yunver <yunus.unver1995@gmail.com>
2019-06-16 17:42:49 +0300

    Added Meal Class(Table Class) Properties

--- YemekTarifleri.Entities/Concrete/Meal.cs
+++ YemekTarifleri.Entities/Concrete/Meal.cs
@@ -10,5 +10,16 @@ namespace YemekTarifleri.Entities.Concrete
     public class Meal:IEntity
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public int  CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CookingTime { get; set; }
+        public string PersonQuantity { get; set; }
+        public string PreparationTime { get; set; }
+        public string Point { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Image { get; set; }
+
     }
 }
--- YemekTarifleri.MvcWebUI/Controllers/MealController.cs
+++ YemekTarifleri.MvcWebUI/Controllers/MealController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using YemekTarifleri.Business.Abstract;
+using YemekTarifleri.MvcWebUI.Models;
 
 namespace YemekTarifleri.MvcWebUI.Controllers
 {
