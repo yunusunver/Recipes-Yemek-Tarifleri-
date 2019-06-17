ec04e9842675b57c37474a872612f3a6eff7db70
yunver <yunus.unver1995@gmail.com>
2019-06-18 01:11:42 +0300

    Added HomeController and HomeViewModel

---
+++ YemekTarifleri.MvcWebUI/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YemekTarifleri.Business.Abstract;
+using YemekTarifleri.MvcWebUI.Models;
+
+namespace YemekTarifleri.MvcWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICategoryService _categoryService;
+
+        private IUserService _userService;
+
+        private IMealService _mealService;
+
+        public HomeController(ICategoryService categoryService, IUserService userService, IMealService mealService)
+        {
+            _categoryService = categoryService;
+            _userService = userService;
+            _mealService = mealService;
+        }
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new HomeViewModel
+            {
+                Categories = _categoryService.GetAll(),
+                Meals = _mealService.GetAll()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ YemekTarifleri.MvcWebUI/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YemekTarifleri.Entities.Concrete;
+
+namespace YemekTarifleri.MvcWebUI.Models
+{
+    public class HomeViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Meal> Meals { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
