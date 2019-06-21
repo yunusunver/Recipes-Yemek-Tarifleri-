5473ef18f47d5cd5cb5f54b898479adc6911ee72
yunver <yunus.unver1995@gmail.com>
2019-06-21 18:24:10 +0300

    Done Login Operations

--- YemekTarifleri.MvcWebUI/Controllers/AccountController.cs
+++ YemekTarifleri.MvcWebUI/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using YemekTarifleri.Business.Abstract;
 using YemekTarifleri.Core.CrossCuttingConcerns.Security.Web;
+using YemekTarifleri.MvcWebUI.Models;
 
 namespace YemekTarifleri.MvcWebUI.Controllers
 {
@@ -17,32 +18,34 @@ namespace YemekTarifleri.MvcWebUI.Controllers
             _userService = userService;
         }
 
-
-        // GET: Account
-        public string Login(string userName,string password)
-        {
-            var user = _userService.GetByUserNameAndPassword(userName, password);
-            if (user != null)
-            {
-                AuthenticationHelper.CreateAuthCookie(
-                    new Guid(), 
-                    user.UserName,
-                    user.Email, 
-                    DateTime.Now.AddDays(15), 
-                    _userService.GetUserRoles(user).Select(x=>x.RoleName).ToArray(),
-                    false, 
-                    user.FirstName, 
-                    user.LastName
-                    );
-                return "Access denied.";
-            }
-
-            return "Not Authenticated";
-        }
-
         public ActionResult Login()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginUser)
+        {
+            var user = _userService.GetByUserNameAndPassword(loginUser.UserName, loginUser.Password);
+            if (user != null)
+            {
+                AuthenticationHelper.CreateAuthCookie(
+                    new Guid(),
+                    user.UserName,
+                    user.Email,
+                    DateTime.Now.AddDays(15),
+                    _userService.GetUserRoles(user).Select(x => x.RoleName).ToArray(),
+                    loginUser.RememberMe,
+                    user.FirstName,
+                    user.LastName
+                );
+                return RedirectToAction("Index","Home");
+            }
+
+            return RedirectToAction("Login");
+        }
+
+
     }
 }
